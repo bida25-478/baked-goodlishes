80d8bac5bdc2cdecd9d1be84648733a96bdeca29
bida25-478 <bida25-478@thuto.bac.ac.bw>
2026-05-20 12:54:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let menu = document.querySelector ('#menu-bar');
+let menu = document.querySelector ('#menu-bars');
 let navbar = document.querySelector ('.navbar');
 
 menu.onclick = () =>{
@@ -6,17 +6,70 @@ menu.onclick = () =>{
     navbar.classList.toggle('active')
 }
 
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
 window.onscroll = () => {
     menu.classList.remove('fa-times')
     navbar.classList.remove('active')
+
+    section.forEach(sec =>{
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop - 150;
+        let id = sec.getAttribute('id');
+
+        if(top=>offset && top<offset+height){
+            navLinks.forEach(links=>{
+                links.classList.remove('active')
+                document.querySelector('hearder .navbara[href*='+id+']').classList.add('active')
+            });
+        };
+    });
 }
 
-function loader(){
-    document.querySelector('.loader-container').classList.add('.fade-out');
+document.querySelector('#search-icon') .onclick =()=>{
+    document.querySelector('#search-form') .classList.toggle('active')
 }
 
-function fadeOut(){
-    setInterval(loader, 3000);
+document.querySelector('#close') .onclick =()=>{
+    document.querySelector('#search-form') .classList.remove('active')
 }
 
-window.onload=fadeOut();
+var swiper = new swiper(".home-slider" ,{
+     spacebetween:30,
+    centeredslides: true,
+    autoplay:{
+        delay:7500,
+        disableoninteraction:false,
+    },
+    pagination:{
+        el:".swiper-pagination",
+        clickable:true,
+    },
+    loop:true,
+});
+
+var swiper = new swiper(".review-slider", {
+     spacebetween:20,
+    centeredslides: true,
+    autoplay:{
+        delay:7500,
+        disableoninteraction:false,
+    },
+    loop:true,
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        640:{
+            slidesPerView:2,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        1024:{
+            slidesPerView:3,
+        },
+    }
+});
